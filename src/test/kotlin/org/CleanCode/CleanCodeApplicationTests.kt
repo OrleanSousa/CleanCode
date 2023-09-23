f8badb4f6d0180597165626b71ae49c53527f92a
OrleanSousa <orleansilva60@hotmail.com>
2023-09-23 16:18:11 -0300

    first Commit

---
+++ src/test/kotlin/org/CleanCode/CleanCodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.CleanCode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanCodeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
